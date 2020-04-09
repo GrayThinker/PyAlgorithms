25ee856d55dd0092f7476369f2b681bca0c82a6f
CreakBandit <42009269+CreakBandit@users.noreply.github.com>
2020-04-08 20:49:58 -0400

    Implement jump search and unit tests for searches

--- searches.py
+++ searches.py
@@ -1,20 +1,67 @@
-# TODO: Jump search, interpolation search, exponential search, sublist search
-# TODO: Fiboonaci search, the ubiquitous binary search, unbound binary search
-# TODO: binary search withouth recursion
+def interpolation_search(val, ls):
+    #TODO: Implement
+    pass
+
+
+def exp_search(val, ls):
+    #TODO: Implement
+    pass
+
+
+def sublist_search(val, ls):
+    #TODO: Implement
+    pass
+
+
+def fib_search(val, ls):
+    #TODO: Implement
+    pass
+
+
+def unbound_binary_search(val, ls):
+    #TODO: Implement
+    pass
+
+
+def ubiq_binary_Search(val, ls):
+    #TODO: Implement
+    pass
+
+
+def rec_binary_Search(val, ls):
+    #TODO: Implement
+    pass
+
 
 def binary_search(val, ls):
     """
-    Use log(n) complexity binary search for val in ls
+    Use O(log(n)) complexity binary search for val in ls
     :param val: value to find in list ls
     :param ls: list of numbers to search through
     :return: index of first instance of val found in ls
     """
-    num = len(ls)
-    half = num // 2
-    cur = ls[half]
-    if cur == val:
-        return half
-    elif val > cur:
-        return half + binary_search(ls[cur:], val)
-    elif val < cur:
-        return binary_search(ls[:cur], val)
+    while len(ls) >= 1:
+        half = len(ls) // 2
+        if val == ls[half]:
+            return half
+        if val > ls[half]:
+            ls = ls[half+1:]
+        else:
+            ls = ls[:half]
+    return -1
+
+print(binary_search(1, [1, 3, 4, 8, 9]))
+
+def jump_search(val, ls):
+    """
+    Use O(sqrt(n))
+    """
+    block = round(len(ls)**.5)
+    for i in range(0, len(ls), block):
+        if ls[i] == val:
+            return i
+        if ls[i] < val:
+            for ii in range(i, block):
+                if ls[ii] == val:
+                    return ii
+    return -1       
